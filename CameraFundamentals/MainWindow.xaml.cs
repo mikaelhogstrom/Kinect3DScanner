6dcd3054fb4f8f713b913eb96c0e7a3190baea8e
Mikael Hogstrom <Micke@Mickes_Dator.(none)>
2012-03-28 16:59:26 +0200

    first commit

---
+++ CameraFundamentals/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿// (c) Copyright Microsoft Corporation.
+// This source is subject to the Microsoft Public License (Ms-PL).
+// Please see http://go.microsoft.com/fwlink/?LinkID=131993 for details.
+// All other rights reserved.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.Diagnostics;
+
+
+namespace CameraFundamentals
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Photo photo;
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+
+            photo = new Photo();
+        }
+      
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //sign up for the event
+            kinectSensorChooser1.KinectSensorChanged += new DependencyPropertyChangedEventHandler(kinectSensorChooser1_KinectSensorChanged);
+
+            
+        }
+
+        void kinectSensorChooser1_KinectSensorChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            var oldSensor = (KinectSensor)e.OldValue;
+
+            //stop the old sensor
+            if (oldSensor != null)
+            {
+                StopKinect(oldSensor); 
+            }
+
+            //get the new sensor
+            var newSensor = (KinectSensor)e.NewValue;
+            if (newSensor == null)
+            {
+                return; 
+            }
+
+            newSensor.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(newSensor_AllFramesReady);
+
+            //turn on features that you need
+            newSensor.ColorStream.Enable(ColorImageFormat.RgbResolution640x480Fps30);                
+            newSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+            newSensor.SkeletonStream.Enable();
+
+            try
+            {
+                newSensor.Start();                
+            }
+            catch (System.IO.IOException)
+            {
+                //this happens if another app is using the Kinect
+                kinectSensorChooser1.AppConflictOccurred();
+            }
+        }
+
+
+        //this event fires when Color/Depth/Skeleton are synchronized
+        void newSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            if (photo.IsTaking)
+            {
+                byte[] colorPixels;
+                short[] depthPixels;
+
+                using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
+                {
+                    if (colorFrame == null) return;
+
+                    colorPixels = new byte[colorFrame.PixelDataLength];
+                    colorFrame.CopyPixelDataTo(colorPixels);
+                }
+
+                using (DepthImageFrame depthFrame = e.OpenDepthImageFrame())
+                {
+                    if (depthFrame == null) return;
+
+                    depthPixels = new short[depthFrame.PixelDataLength];
+                    depthFrame.CopyPixelDataTo(depthPixels);
+                }
+
+                photo.AddFrame(colorPixels, depthPixels);
+            }
+        }
+
+
+        private void StopKinect(KinectSensor sensor)
+        {
+            if (sensor != null)
+            {
+                if (sensor.IsRunning)
+                {
+                    //stop sensor 
+                    sensor.Stop();
+
+                    //stop audio if not null
+                    if (sensor.AudioSource != null)
+                    {
+                        sensor.AudioSource.Stop();
+                    }
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            StopKinect(kinectSensorChooser1.Kinect); 
+        }
+
+        private void btnTakePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            photo.TakePhoto(2, 640, 480, imgColorPhoto, imgDepthPhoto);
+        } 
+    }
+}
---
+++ CameraFundamentals/Photo.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls;
+
+namespace CameraFundamentals
+{
+    class Photo
+    {
+        private const int PLAYER_INDEX_BITMASK_WIDTH = 3;
+        const float MaxDepthDistance = 4095; // max value returned
+        const float MinDepthDistance = 850; // min value returned
+        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
+
+        //TODO use only this until averaging works
+        private const int FRAME_USED = 0;
+        
+        private bool isTaking = false;
+        private int currentFrame;
+        private int totalFrames;
+        private Image colorRenderTarget;
+        private Image depthRenderTarget;
+        private int width;
+        private int height;
+
+        private byte[][] colorPixels;
+        private byte[][] depthPixels;
+        private short[][] depthData;
+
+        public bool IsTaking
+        {
+            get { return isTaking; }
+        }
+
+        
+
+        public void TakePhoto(int Frames, int Width, int Height, Image ColorRenderTarget, Image DepthRenderTarget)
+        {
+            colorPixels = new byte[Frames][];
+            depthData = new short[Frames][];
+            depthPixels = new byte[Frames][];
+            isTaking = true;
+            currentFrame = 0;
+            totalFrames = Frames;
+            width = Width;
+            height = Height;
+
+            colorRenderTarget = ColorRenderTarget;
+            depthRenderTarget = DepthRenderTarget;
+        }
+
+        public void AddFrame(byte[] color, short[] depth)
+        {
+            colorPixels[currentFrame] = color;
+            depthData[currentFrame] = depth;
+            currentFrame++;
+
+            if (currentFrame >= totalFrames)
+            {
+                isTaking = false;
+                finalizeImage();
+            }
+        }
+
+        /// <summary>
+        /// Processes the captured frames into one image and one 
+        /// </summary>
+        private void finalizeImage()
+        {
+            translateDepthToPixels();
+            //TODO: when averaging make a similarity function to check if colordata is consistent through frames
+            // if it is not the picture should be rejected
+            int stride = width * 4;
+            colorRenderTarget.Source =
+                BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, colorPixels[FRAME_USED], stride);
+            depthRenderTarget.Source =
+                BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgr32, null, depthPixels[FRAME_USED], stride);
+        }
+
+        private void translateDepthToPixels()
+        {
+            byte[] pixels = new byte[height*width*4];
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            //loop through all distances
+            //pick a RGB color based on distance
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < depthData[FRAME_USED].Length && colorIndex < pixels.Length;
+                depthIndex++, colorIndex += 4)
+            {
+                //Get the depth value in mm
+                //It seems that three bits are used for player detection in some whay so we account for that
+                //by bitshifting
+                int depth = 0;
+                for (int i = 0; i < totalFrames; i++)
+                {
+                    depth += depthData[i][depthIndex] >> PLAYER_INDEX_BITMASK_WIDTH;    
+                }
+                depth /= totalFrames;
+                
+                //TODO: Optimize this calculation!
+                byte intensity = (byte)(255 - (255 * Math.Max(depth - MinDepthDistance, 0)
+                / (MaxDepthDistanceOffset)));
+
+                //equal coloring for monochromatic
+                pixels[colorIndex + BlueIndex] = intensity;
+                pixels[colorIndex + GreenIndex] = intensity;
+                pixels[colorIndex + RedIndex] = intensity;
+            }
+
+            //TODO create an averaging scheme...
+            depthPixels[FRAME_USED] = pixels;
+
+        }
+
+    }
+}
